83322da6f06cb41d9fe5c6171354f71f9bdf92b5
roneyyb <bothra.rajat08@gmail.com>
2020-07-10 17:41:38 +0530

    Little changes

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+    res.status(200).send("Home page");
+});
+
 app.listen(3000, () => {
     console.log("Application started on port 3000");
 })
